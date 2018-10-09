c6085a17e27e5c4895c3f4579c82095f96a982b3
Enrique Devars <e.devars.dev@gmail.com>
2018-10-09 16:37:57 -0500

    Modifying the values for any point in the functions

--- Scripts/Ejercicios/Ejercicio 1/Ejercicio 1.4/ejercicio1_4f.m
+++ Scripts/Ejercicios/Ejercicio 1/Ejercicio 1.4/ejercicio1_4f.m
@@ -2,19 +2,18 @@ clc;
 clear all;
 close all;
 
-syms A wo t T;
+syms A wo t T to;
 
 
 % 6)    Calculo de los valores pedidos para la senial 6
 
+f6=(A/T).*(t-to);
 
-f6=(A/T).*(t);
+f6_vprom=vprom(f6,to,to+T,T,t);
 
-f6_vprom=vprom(f6,0,T,T,t);
+f6_potencia=potencia(f6,to,to+T,T,t);
 
-f6_potencia=potencia(f6,0,T,T,t);
-
-f6_vrms=vrms(f6,0,T,T,t);
+f6_vrms=vrms(f6,to,to+T,T,t);
 
 fprintf('\n------------Funcion 6-------------\n')
 
--- Scripts/Ejercicios/Ejercicio 1/Ejercicio 1.4/ejercicio1_4g.m
+++ Scripts/Ejercicios/Ejercicio 1/Ejercicio 1.4/ejercicio1_4g.m
@@ -7,12 +7,15 @@ syms A t T;
 
 % 7) Calculo de los valores pedidos para la senial 7
 
- 
-f7_vprom=vprom((2.*A/T).*t,0,T/2,T,t)+vprom(-((2.*A)/T).*(t-T),T/2,T,T,t);
+f7a=(2.*A/T).*t;
 
-f7_vrms=vrms((2.*A/T).*t,0,T/2,T,t)+vrms(-((2.*A)/T).*(t-T),T/2,T,T,t);
+f7b=-((2.*A)/T).*(t-T);
 
-f7_potencia=potencia((2.*A/T).*t,0,T/2,T,t)+potencia(((2.*A)/T).*(t-T),T/2,T,T,t);
+f7_vprom=vprom((2.*A/T).*t,0,T/2,T,t)+vprom(-((2.*A)/T).*(t)+2.*A,T/2,T,T,t);
+
+f7_vrms=sqrt((1/T).*(int(f7a.^2,t,0,T/2)+int(f7b.^2,t,T/2,T)));
+
+f7_potencia=potencia((2.*A/T).*t,0,T/2,T,t)+potencia(-((2.*A)/T).*(t)+2.*A,T/2,T,T,t);
 
 fprintf('\n------------Funcion 7-------------\n')
 
